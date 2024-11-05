811c9de36a7c0875ae0a501168ccaa3b2c3fbca0
Nikita Solntsev <93279784+NikitaSolntsev@users.noreply.github.com>
2024-11-05 21:54:43 +0300

    🎉 Begin a project

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import {Button} from "@nextui-org/react";
+
+export default function Home() {
+  return (
+    <div>
+      <Button color="primary">
+        Button
+      </Button>
+    </div>
+  );
+}
